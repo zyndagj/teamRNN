2cf944a7e153e014f183b6accdbbc55f0b55d093
Greg Zynda <gzynda@staff.stampede2.tacc.utexas.edu>
2018-07-17 13:31:08 -0500

    Committing changes

--- teamRNN/reader.py
+++ teamRNN/reader.py
@@ -4,6 +4,8 @@ from pysam import FastaFile
 from multiprocessing import Pool, RawArray, cpu_count
 from string import maketrans
 from array import array
+import subprocess as sp
+import numpy as np
 
 class refcache:
 	def __init__(self, FA, chrom, chromLen, cacheSize=50000):
@@ -31,10 +33,87 @@ def isOdd(num):
 def isEven(num):
 	return num % 2 == 0
 
-#class methParser:
-#	def __init__(self, fastaFile, methFile, windowSize, slideSize):
-#	def getArray(self, chrom):
-#	def get(self, chrom, index):
+#contextDict = {'+':{'CG':6, 'CHG':8, 'CHH':10}, '-':{'CG':7, 'CHG':9, 'CHH':11}}
+def methHelper(line):
+	tmp = line.split('\t')
+	index = int(tmp[1])-1
+	C, CT = map(int, tmp[6:8])
+	sign, context = tmp[2:4]
+	mDict[sign][context][index] = C
+	tDict[sign][context][index] = CT
+class methParser:
+	def __init__(self, fastaFile, methFile, windowSize, slideSize=1):
+		# Load fasta
+		self.FA = FastaFile(fastaFile)
+		self.chromosomes = sorted(self.FA.references)
+		self.methFile = methFile
+		self.windowSize = windowSize
+		# Always use a slide size of 1 to match kmer data
+		self.slideSize = slideSize
+	def genOutput(self, chrom):
+		chromLen = self.FA.get_reference_length(chrom)
+		down = (self.windowSize-1)/2
+		for i in xrange(chromLen):
+			s = i-down
+			e = i+down+1
+			if s < 0:
+				I = range(abs(s), 0, -1)+range(0, e)
+			elif e >= chromLen:
+				diff = e-chromLen
+				I = range(s, chromLen)+range(chromLen-diff-1, chromLen-1)
+			else:
+				I = range(s,e)
+			# Print CHH
+			print [t+1 for t in I]
+			for context in ('CG', 'CHG', 'CHH'):
+				for strand in ('+','-'):
+					# Calc these
+			mp = sum([mCHHp[t] for t in I])
+			tp = sum([tCHHp[t] for t in I])
+			mn = sum([mCHHn[t] for t in I])
+			tn = sum([tCHHn[t] for t in I])
+			print chrom, i+1, "%i/%i+\t%i/%i-"%(mp,tp,mn,tn)
+	def getArray(self, chrom, nProcs=0):
+		# Make sure chromosome is valid
+		if chrom not in self.chromosomes:
+			raise ValueError("%s not in %s"%(chrom, self.FA.filename))
+		chromLen = self.FA.get_reference_length(chrom)
+		# mC*p for positive strand
+		global mCGp, tCGp, mCHGp, tCHGp, mCHHp, tCHHp
+		mCGp = RawArray('h', [0]*chromLen)
+		tCGp = RawArray('h', [0]*chromLen)
+		mCHGp = RawArray('h', [0]*chromLen)
+		tCHGp = RawArray('h', [0]*chromLen)
+		mCHHp = RawArray('h', [0]*chromLen)
+		tCHHp = RawArray('h', [0]*chromLen)
+		# mC*n for negative string
+		global mCGn, tCGn, mCHGn, tCHGn, mCHHn, tCHHn
+		mCGn = RawArray('h', [0]*chromLen)
+		tCGn = RawArray('h', [0]*chromLen)
+		mCHGn = RawArray('h', [0]*chromLen)
+		tCHGn = RawArray('h', [0]*chromLen)
+		mCHHn = RawArray('h', [0]*chromLen)
+		tCHHn = RawArray('h', [0]*chromLen)
+		global mDict, tDict
+		mDict = {'+':{'CG':mCGp, 'CHG':mCHGp, 'CHH':mCHHp}, '-':{'CG':mCGn, 'CHG':mCHGn, 'CHH':mCHHn}}
+		tDict = {'+':{'CG':tCGp, 'CHG':tCHGp, 'CHH':tCHHp}, '-':{'CG':tCGn, 'CHG':tCHGn, 'CHH':tCHHn}}
+		# Initialize workers
+		N = nProcs if nProcs else cpu_count()
+		p = Pool(N)
+		# Filter file and process
+		chromLines = sp.Popen('LC_ALL=C grep "^%s\s" %s'%(chrom, self.methFile), shell=True, stdout=sp.PIPE).stdout
+		ret = p.map_async(methHelper, chromLines, 100)
+		# Close pool
+		p.close()
+		p.join()
+		# Stack arrays and return
+		self.genOutput(chrom)
+		#outArray = np.hstack((mCG, tCG, mCHG, tCHG, mCHH, tCHH))
+		# Delete shared arrays
+		del mCGp, tCGp, mCHGp, tCHGp, mCHHp, tCHHp
+		del mCGn, tCGn, mCHGn, tCHGn, mCHHn, tCHHn
+		del mDict, tDict
+		return outArray
 
 b2i = maketrans('AGCT','0123')
 def kmer2index(kmer):
--- tests/__init__.py
+++ tests/__init__.py
@@ -11,6 +11,7 @@ class TestReader(unittest.TestCase):
 	def setUp(self):
 		self.fasta = path.join(path.dirname(__file__),'test.fa')
 		self.RP = reader.refParser(self.fasta, 1, 5)
+		self.methFile = path.join(path.dirname(__file__),'test1_methratio.txt')
 	def test_odd(self):
 		self.assertTrue(reader.isOdd(5))
 		self.assertFalse(reader.isOdd(4))
@@ -52,6 +53,30 @@ class TestReader(unittest.TestCase):
 	def test_array_Chr2(self):
 		chr2A = 'ATCTAGCTAGCTAGCTAGAT'
 		self.assertEqual(self.RP.wrapBases('Chr2'), (16, 20, chr2A))
+	def test_meth_array_Chr1(self):
+		self.MF = reader.methParser(self.fasta, self.methFile, 5)
+		self.MF.getArray('Chr1')
+		
+#		chr     pos     strand  context ratio   eff_CT  C       CT
+#		Chr1    5       -       CHH     0.0     0.0     5       7
+#		Chr1    6       -       CHH     0.0     0.0     0       0
+#		Chr1    7       -       CHH     0.0     0.0     3       7
+#		Chr1    8       -       CHH     0.0     0.0     2       7
+#		Chr1    9       +       CHH     0.0     0.0     5       5
+#		Chr1    10      +       CHH     0.0     0.0     4       7
+#		Chr1    11      +       CHH     0.0     0.0     2       2
+#		Chr1    12      +       CHH     0.0     0.0     0       7
+#		Chr2    1       +       CG      0.0     0.0     1       9
+#		Chr2    2       -       CG      0.0     0.0     1       2
+#		Chr2    5       +       CG      0.0     0.0     6       7
+#		Chr2    6       -       CG      0.0     0.0     8       8
+#		Chr2    9       +       CG      0.0     0.0     7       8
+#		Chr2    10      -       CG      0.0     0.0     2       3
+#		Chr2    13      +       CG      0.0     0.0     1       1
+#		Chr2    14      -       CG      0.0     0.0     2       2
+#		Chr2    15      -       CHG     0.0     0.0     1       5
+		
+
 
 if __name__ == "__main__":
 	unittest.main()
