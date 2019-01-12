fec74a5473c50771dfd8e8583e1e69cf1e245495
Greg Zynda <zyndagj@gmail.com>
2019-01-11 22:53:11 -0600

    Commiting changes

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,82 +1,46 @@
-import unittest, sys
+import unittest, sys, os
+try:
+	from StringIO import StringIO
+except:
+	from io import StringIO
+# buffer for capturing log info
+logStream = StringIO()
+# Need to start logger BEFORE importing any pyPlateCalibrate code
+import logging
+FORMAT = "[%(levelname)s - %(filename)s:%(lineno)s - %(funcName)15s] %(message)s"
+logging.basicConfig(stream=logStream, level=logging.DEBUG, format=FORMAT)
 from teamRNN import reader
+from pysam import FastaFile
 #try:
 #	from unittest.mock import patch
 #except:
 #	from mock import patch
-#from StringIO import StringIO
-from os import path
 
 class TestReader(unittest.TestCase):
 	def setUp(self):
-		self.fasta = path.join(path.dirname(__file__),'test.fa')
-		self.RP = reader.refParser(self.fasta, 1, 5)
-		self.methFile = path.join(path.dirname(__file__),'test1_methratio.txt')
-	def test_odd(self):
-		self.assertTrue(reader.isOdd(5))
-		self.assertFalse(reader.isOdd(4))
-		self.assertFalse(reader.isOdd(0))
-		self.assertTrue(reader.isOdd(-1))
-	def test_even(self):
-		self.assertTrue(reader.isEven(4))
-		self.assertFalse(reader.isEven(5))
-		self.assertTrue(reader.isEven(0))
-		self.assertFalse(reader.isEven(-1))
-	def test_kmer2index(self):
-		# AGAG
-		# 0101
-		self.assertEqual(reader.kmer2index('AGAG'), 17)
-		self.assertEqual(reader.kmer2index('AGAN'), -1)
-		self.assertEqual(reader.kmer2index('N'), -1)
-	def test_up(self):
-		self.assertEqual(self.RP.up, 2)
-	def test_down(self):
-		self.assertEqual(self.RP.down, 2)
-	def test_array_Chr1(self):
-		chr1A = 'AAAAAAGGGGCCCCTTTTTT'
-		self.assertEqual(self.RP.wrapBases('Chr1'), (16, 20, chr1A))
-	def test_kmer_worker(self):
-		cLen, wrapLen, wrapChr1A = self.RP.wrapBases('Chr1')
-		#chr1A = 'AAAAAAGGGGCCCCTTTTTT'
-		chr1A = [0,0,1,5,21,85,342,346,362,426,683,687,703,767,1023,1023]
-		reader.baseArray = wrapChr1A
-		reader.kmerArray = [0]*cLen
-		reader.kmer_worker((self.RP.k, 0, 1))
-		self.assertEqual(reader.kmerArray, chr1A)
-		del reader.baseArray, reader.kmerArray
-	def test_kmer_Chr1(self):
-		#chr1A = 'AAAAAAGGGGCCCCTTTTTT'
-		#chr1A = '00000011112222333333'
-		chr1A = [0,0,1,5,21,85,342,346,362,426,683,687,703,767,1023,1023]
-		self.assertEqual(list(self.RP.getArray('Chr1',4)), chr1A)
-		self.assertEqual(self.RP.FA.get_reference_length('Chr1'), len(chr1A))
-	def test_array_Chr2(self):
-		chr2A = 'ATCTAGCTAGCTAGCTAGAT'
-		self.assertEqual(self.RP.wrapBases('Chr2'), (16, 20, chr2A))
-	def test_meth_array_Chr1(self):
-		self.MF = reader.methParser(self.fasta, self.methFile, 5)
-		self.MF.getArray('Chr1')
-		
-#		chr     pos     strand  context ratio   eff_CT  C       CT
-#		Chr1    5       -       CHH     0.0     0.0     5       7
-#		Chr1    6       -       CHH     0.0     0.0     0       0
-#		Chr1    7       -       CHH     0.0     0.0     3       7
-#		Chr1    8       -       CHH     0.0     0.0     2       7
-#		Chr1    9       +       CHH     0.0     0.0     5       5
-#		Chr1    10      +       CHH     0.0     0.0     4       7
-#		Chr1    11      +       CHH     0.0     0.0     2       2
-#		Chr1    12      +       CHH     0.0     0.0     0       7
-#		Chr2    1       +       CG      0.0     0.0     1       9
-#		Chr2    2       -       CG      0.0     0.0     1       2
-#		Chr2    5       +       CG      0.0     0.0     6       7
-#		Chr2    6       -       CG      0.0     0.0     8       8
-#		Chr2    9       +       CG      0.0     0.0     7       8
-#		Chr2    10      -       CG      0.0     0.0     2       3
-#		Chr2    13      +       CG      0.0     0.0     1       1
-#		Chr2    14      -       CG      0.0     0.0     2       2
-#		Chr2    15      -       CHG     0.0     0.0     1       5
-		
-
+		tpath = os.path.dirname(__file__)
+		self.fa = os.path.join(tpath, 'test.fa')
+		self.fai = os.path.join(tpath, 'test.fa.fai')
+		self.gff3 = os.path.join(tpath, 'test.gff')
+		self.mr1 = os.path.join(tpath, 'test1_methratio.txt')
+		self.mr2 = os.path.join(tpath, 'test2_methratio.txt')
+	def tearDown(self):
+		## Runs after every test function ##
+		# Wipe log
+		logStream.truncate(0)
+		## Runs after every test function ##
+	def test_refcache(self):
+		FA = FastaFile(self.fa)
+		for chrom in FA.references:
+			chrom_len = FA.get_reference_length(chrom)
+			chrom_seq = FA.fetch(chrom)
+			rc = reader.refcache(self.fa, chrom)
+			self.assertEqual(chrom_seq, rc.fetch(0, chrom_len))
+			for i in range(chrom_len-3):
+				self.assertEqual(rc.fetch(i, i+3), FA.fetch(chrom, i, i+3))
+		# clean up
+		FA.close()
+		del rc
 
 if __name__ == "__main__":
 	unittest.main()
